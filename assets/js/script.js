f166c3ecb56dde5eb6e98725cab47b1f18be9062
Leobardo Daniel Miramontes Murillo <ret.hatsu@gmail.com>
2022-10-18 18:16:48 -0500

    Se termino todo lo de diseño  faltan validaciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ document.getElementById("btn_login_al").addEventListener("click", registro_alumn
 document.getElementById("btn_login_do").addEventListener("click", registro_docentes);
 document.getElementById("btn_login_ex").addEventListener("click", registro_externos);
 document.getElementById("btn_login").addEventListener("click", mostrar_login);
+
+window.addEventListener("resize", anchoPagina); //esto controla el efecto de la ventena
+
 //declaracion de variables
 var contenedor_Form = document.querySelector(".contenedor_formularios");
 
@@ -15,46 +18,132 @@ var formulario_registro_al = document.querySelector(".formulario_registro_alumno
 var formulario_registro_do = document.querySelector(".formulario_registro_docentes");
 var formulario_registro_ex = document.querySelector(".formulario_registro_externos");
 
+//funcion para que la curp y el rfc no acepten caracteres especiales
+function check(e) {
+    tecla = (document.all) ? e.keyCode : e.which;
+
+    //Tecla de retroceso para borrar, siempre la permite
+    if (tecla == 8) {
+        return true;
+    }
+
+    // Patrón de entrada, en este caso solo acepta numeros y letras
+    patron = /[A-Za-z0-9]/;
+    tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
+}
+
+
+/**Funcion hecha para automatizar el responsive */
+function anchoPagina(){
+    if (window.innerWidth > 850){
+        lim_Login.style.display="block";
+        lim_Reg.style.display="block";
+    }
+    else{
+        lim_Reg.style.display="block";
+        lim_Reg.style.opacity="1";
+        lim_Login.style.display="none";
+        formulario_login.style.display="block";
+        formulario_registro_al.style.display="none";
+        formulario_registro_do.style.display="none";
+        formulario_registro_ex.style.display="none";
+        contenedor_Form.style.left="0px";
+    }
+}
+
+anchoPagina(); //hace que la funcion se ejecute cuando se recarga la pagina
+
+/* estos controlan la animacion login registro, estan configurados para actuar cuando la pagina se hace pequeña */
 function mostrar_login(){
-    formulario_login.style.display="block";
-    formulario_registro_al.style.display = "none";
-    formulario_registro_do.style.display = "none";
-    formulario_registro_ex.style.display = "none";
-    contenedor_Form.style.left = "10px";
-    formulario_login.style.display="block";
-    lim_Reg.style.opacity="1";
-    lim_Login.style.opacity="0";
+    if(window.innerWidth>850){
+        formulario_login.style.display="block";
+        formulario_registro_al.style.display = "none";
+        formulario_registro_do.style.display = "none";
+        formulario_registro_ex.style.display = "none";
+        contenedor_Form.style.left = "10px";
+        formulario_login.style.display="block";
+        lim_Reg.style.opacity="1";
+        lim_Login.style.opacity="0";
+    }
+    else{
+        formulario_login.style.display="block";
+        formulario_registro_al.style.display = "none";
+        formulario_registro_do.style.display = "none";
+        formulario_registro_ex.style.display = "none";
+        contenedor_Form.style.left = "0px";
+        formulario_login.style.display="block";
+        lim_Reg.style.display="block";
+        lim_Login.style.display="none";
+    }
+
+    
 }
 
 function registro_alumnos(){
-    formulario_registro_al.style.display = "block";
-    formulario_registro_do.style.display = "none";
-    formulario_registro_ex.style.display = "none";
-    contenedor_Form.style.left = "480px";
-    formulario_login.style.display="none";
-    lim_Reg.style.opacity="0";
-    lim_Login.style.opacity="1";
+    if(window.innerWidth>850){
+        formulario_registro_al.style.display = "block";
+        formulario_registro_do.style.display = "none";
+        formulario_registro_ex.style.display = "none";
+        contenedor_Form.style.left = "480px";
+        formulario_login.style.display="none";
+        lim_Reg.style.opacity="0";
+        lim_Login.style.opacity="1";
+    }
+    else{
+        formulario_registro_al.style.display = "block";
+        formulario_registro_do.style.display = "none";
+        formulario_registro_ex.style.display = "none";
+        contenedor_Form.style.left = "0px";
+        formulario_login.style.display="none";
+        lim_Reg.style.display="none";
+        lim_Login.style.display="block";
+        lim_Login.style.opacity="1";
+    }
 
 }
 
 function registro_docentes(){
-    formulario_registro_al.style.display = "none";
-    formulario_registro_do.style.display = "block";
-    formulario_registro_ex.style.display = "none";
-    contenedor_Form.style.left = "480px";
-    formulario_login.style.display="none";
-    lim_Reg.style.opacity="0";
-    lim_Login.style.opacity="1";
+    if(window.innerWidth>850){
+        formulario_registro_al.style.display = "none";
+        formulario_registro_do.style.display = "block";
+        formulario_registro_ex.style.display = "none";
+        contenedor_Form.style.left = "480px";
+        formulario_login.style.display="none";
+        lim_Reg.style.opacity="0";
+        lim_Login.style.opacity="1";
+    }
+    else{
+        formulario_registro_al.style.display = "block";
+        formulario_registro_do.style.display = "none";
+        formulario_registro_ex.style.display = "none";
+        contenedor_Form.style.left = "0px";
+        formulario_login.style.display="none";
+        lim_Reg.style.display="none";
+        lim_Login.style.display="block";
+        lim_Login.style.opacity="1";
+    }
 
 }
 
 function registro_externos(){
-    formulario_registro_al.style.display = "none";
-    formulario_registro_do.style.display = "none";
-    formulario_registro_ex.style.display = "block";
-    contenedor_Form.style.left = "480px";
-    formulario_login.style.display="none";
-    lim_Reg.style.opacity="0";
-    lim_Login.style.opacity="1";
-
+    if(window.innerWidth>850){
+        formulario_registro_al.style.display = "none";
+        formulario_registro_do.style.display = "none";
+        formulario_registro_ex.style.display = "block";
+        contenedor_Form.style.left = "480px";
+        formulario_login.style.display="none";
+        lim_Reg.style.opacity="0";
+        lim_Login.style.opacity="1";
+    }
+    else{
+        formulario_registro_al.style.display = "block";
+        formulario_registro_do.style.display = "none";
+        formulario_registro_ex.style.display = "none";
+        contenedor_Form.style.left = "0px";
+        formulario_login.style.display="none";
+        lim_Reg.style.display="none";
+        lim_Login.style.display="block";
+        lim_Login.style.opacity="1";
+    }
 }
