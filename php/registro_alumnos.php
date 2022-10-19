b558e37cf4edf047a9107164d576359392c42033
Leobardo Daniel Miramontes Murillo <ret.hatsu@gmail.com>
2022-10-18 21:44:44 -0500

    Evitar duplicados en la BD

--- php/registro_alumnos.php
+++ php/registro_alumnos.php
@@ -1,13 +1,43 @@
 <?php
 
+    include("conexion_be.php");
+
+    $ban=false;
     $nombres=$_POST['nombres_al'];
-    $ap_paterno=$_POST['ap_paterno_al'];
-    $ap_materno=$_POST['ap_materno_al'];
+    $ap_paterno=$_POST['ap_pat_al'];
+    $ap_materno=$_POST['ap_mat_al'];
     $matricula=$_POST['num_control_al'];
     $semestre=$_POST['semestre_al'];
     $email=$_POST['email_al'];
-    $telefono=$_POST['telefono_al'];
+    $telefono=$_POST['tel_al'];
     $password=$_POST['pass1_al'];
     $password2=$_POST['pass2_al'];
     $participacion=$_POST['participacion_al'];
-    echo $nombres.$ap_paterno.$ap_materno.$matricula.$semestre.$email.$telefono.$password.$password2.$participacion;
+    //aseguramos que no haya campos vacios
+    if($nombres == "" || $ap_paterno == "" || $ap_materno == "" || $matricula == "" || $semestre == "" || $email == "" || $telefono == "" || $password == "" || $password2 == "" || $participacion == ""){
+        echo '<script>alert("No se permiten campos vacios")</script>';
+    }else{
+        $ban=true;
+    }    
+    //Aqui se hace el cambio a una varable int para poder insertarla en la base de datos
+
+    if ($participacion=="oyente"){
+        $participacion=0;
+    }
+    else{
+        $participacion=1;
+    }
+    //echo $nombres.$ap_paterno.$ap_materno.$matricula.$semestre.$email.$telefono.$password.$password2.$participacion;
+
+
+    if ($password==$password2 && $ban==true){
+        $query="INSERT INTO alumnos (nombres, ap_paterno, ap_materno, nc, semestre, email_al, pass, participacion, telefono) 
+        VALUES ('$nombres', '$ap_paterno', '$ap_materno', '$matricula', '$semestre', '$email', '$password', '$participacion', '$telefono')";
+        $ejecutar = mysqli_query($conexion, $query);
+        if($ejecutar){
+            echo '<script>alert("Te has registrado correctamente, ya puedes iniciar sesion");window.location="index.php";</script>';
+        }
+    }
+    else{
+        echo '<script>alert("Las contraseñas no coinciden")</script>';
+    }
--- registro_alumnos.php
+++ registro_alumnos.php
@@ -2,6 +2,7 @@
 
     include("conexion_be.php");
 
+    $ban=false;
     $nombres=$_POST['nombres_al'];
     $ap_paterno=$_POST['ap_pat_al'];
     $ap_materno=$_POST['ap_mat_al'];
@@ -12,6 +13,12 @@
     $password=$_POST['pass1_al'];
     $password2=$_POST['pass2_al'];
     $participacion=$_POST['participacion_al'];
+    //aseguramos que no haya campos vacios
+    if($nombres == "" || $ap_paterno == "" || $ap_materno == "" || $matricula == "" || $semestre == "" || $email == "" || $telefono == "" || $password == "" || $password2 == "" || $participacion == ""){
+        echo '<script>alert("No se permiten campos vacios");window.location="index.php";</script>';
+    }else{
+        $ban=true;
+    }    
     //Aqui se hace el cambio a una varable int para poder insertarla en la base de datos
 
     if ($participacion=="oyente"){
@@ -22,13 +29,33 @@
     }
     //echo $nombres.$ap_paterno.$ap_materno.$matricula.$semestre.$email.$telefono.$password.$password2.$participacion;
 
-
-    if ($password==$password2){
+    //verificar que el correo y el nc no este registrado
+    $verificar_nc= mysqli_query($conexion, "SELECT * FROM alumnos WHERE nc='$matricula'");
+    if (mysqli_num_rows($verificar_nc)>0){
+        echo '<script>alert("Este numero de control ya está registrado");window.location="index.php";</script>';
+        exit();
+        mysqli_close($conexion);
+    }
+    $verificar_correo= mysqli_query($conexion, "SELECT * FROM alumnos WHERE email_al='$email'");
+    if (mysqli_num_rows($verificar_correo)>0){
+        echo '<script>alert("Este correo ya está registrado, intenta con otro diferente");window.location="index.php";</script>';
+        exit();
+        mysqli_close($conexion);
+    }
+    //ejecutar la consulta si 
+    if ($password==$password2 && $ban==true){
         $query="INSERT INTO alumnos (nombres, ap_paterno, ap_materno, nc, semestre, email_al, pass, participacion, telefono) 
         VALUES ('$nombres', '$ap_paterno', '$ap_materno', '$matricula', '$semestre', '$email', '$password', '$participacion', '$telefono')";
         $ejecutar = mysqli_query($conexion, $query);
-
-        echo '<script>alert("Te has registrado correctamente, ya puedes iniciar sesion")</script>';
-    }else{
-        echo '<script>alert("Las contraseñas no coinciden")</script>';
+        if($ejecutar){
+            echo '<script>alert("Te has registrado correctamente, ya puedes iniciar sesion");window.location="index.php";</script>';
+        }
+        else{
+            echo '<script>alert("No se pudo registrar, intentalo de nuevo");window.location="index.php";</script>';
+        }
     }
+    else{
+        echo '<script>alert("Las contraseñas no coinciden");window.location="index.php";</script>';
+    }
+    mysqli_close($conexion);
+?>
